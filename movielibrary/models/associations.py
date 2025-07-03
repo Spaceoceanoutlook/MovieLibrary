3b3ff3864e5114d1daeabb4d8806c7c0d8e47693
spaceoceanoutlook <spaceocean@outlook.com>
2025-07-03 20:58:01 +0500

    changed the films table

--- movielibrary/models/associations.py
+++ movielibrary/models/associations.py
@@ -1,7 +1,12 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base
 from sqlalchemy import ForeignKey
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from .film import Film
+    from .genre import Genre
+    from .country import Country
 
 class FilmGenre(Base):
     __tablename__ = "film_genre"
@@ -9,10 +14,8 @@ class FilmGenre(Base):
     film_id: Mapped[int] = mapped_column(ForeignKey("films.id"), primary_key=True)
     genre_id: Mapped[int] = mapped_column(ForeignKey("genres.id"), primary_key=True)
 
-    # 2. ИСПОЛЬЗУЕМ СТРОКИ ВМЕСТО КЛАССОВ
     film: Mapped["Film"] = relationship(back_populates="genres")
     genre: Mapped["Genre"] = relationship(back_populates="films")
-
 
 class FilmCountry(Base):
     __tablename__ = "film_country"
@@ -20,6 +23,5 @@ class FilmCountry(Base):
     film_id: Mapped[int] = mapped_column(ForeignKey("films.id"), primary_key=True)
     country_id: Mapped[int] = mapped_column(ForeignKey("countries.id"), primary_key=True)
 
-    # 2. ИСПОЛЬЗУЕМ СТРОКИ ВМЕСТО КЛАССОВ
     film: Mapped["Film"] = relationship(back_populates="countries")
     country: Mapped["Country"] = relationship(back_populates="films")
--- movielibrary/models/film.py
+++ movielibrary/models/film.py
@@ -1,6 +1,6 @@
 from typing import List
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer
+from sqlalchemy import String, Integer, Float
 from .base import Base
 from .associations import FilmGenre, FilmCountry
 
@@ -11,7 +11,7 @@ class Film(Base):
     title: Mapped[str] = mapped_column(String, nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
     description: Mapped[str | None] = mapped_column(String, nullable=True)
-    rating: Mapped[int] = mapped_column(Integer, nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
     photo: Mapped[str] = mapped_column(String, nullable=False)
 
     genres: Mapped[List["FilmGenre"]] = relationship(back_populates="film", cascade="all, delete-orphan")

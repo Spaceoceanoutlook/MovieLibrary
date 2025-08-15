536d313074c30d374d775b44edfea18a60d924e6
spaceoceanoutlook <spaceocean@outlook.com>
2025-08-15 10:03:49 +0500

    rename app.py to main.py

--- movielibrary/main.py
+++ movielibrary/main.py
@@ -11,4 +11,4 @@ app.include_router(pages.router, tags=["Web Pages"], include_in_schema=False)
 
 
 if __name__ == "__main__":
-    uvicorn.run("movielibrary.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("movielibrary.main:app", host="0.0.0.0", port=8000, reload=True)

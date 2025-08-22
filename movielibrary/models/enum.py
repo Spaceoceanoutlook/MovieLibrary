5dafe7946ac2b9c80b234842c9f20d0604970990
Spaceoceanoutlook <spaceocean@outlook.com>
2025-08-22 13:16:00 +0500

    added column type in film.py and alembic migration

---
+++ movielibrary/models/enum.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class MediaType(StrEnum):
+    movie = "movie"
+    series = "series"
--- movielibrary/models/film.py
+++ movielibrary/models/film.py
@@ -1,10 +1,11 @@
 from typing import List
 
-from sqlalchemy import Float, Integer, String
+from sqlalchemy import Enum, Float, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .associations import FilmCountry, FilmGenre
 from .base import Base
+from .enum import MediaType
 
 
 class Film(Base):
@@ -12,6 +13,7 @@ class Film(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String, nullable=False)
+    type: Mapped[str] = mapped_column(Enum(MediaType), default=MediaType.movie)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
     description: Mapped[str | None] = mapped_column(String, nullable=True)
     rating: Mapped[float] = mapped_column(Float, nullable=False)
